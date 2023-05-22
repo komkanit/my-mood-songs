419ee80d958825ec60b91247c7b976e448744101
komcal <kom.sujautra@gmail.com>
2023-05-22 19:18:19 +0700

    fix bug

--- components/listMoods/Mood.tsx
+++ components/listMoods/Mood.tsx
@@ -1,31 +1,23 @@
-import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useRef, useState } from "react";
-import { random } from "../../lib/helper";
-import { CustomFeelingType } from "../../pages/moods";
 import Image from "next/image";
 import { useClickOutside } from "../../lib/hook/useClickOutside";
+import { FeelingType } from "../../lib/moodHelper";
 
-const randomNumber = () => {
-    const numbers = [300, 400, 500, 600, 700];
-    return random(0, numbers.length - 1);
-}
-
-export default function Mood({feeling, index}: { index: number, feeling: CustomFeelingType }) {
-    const [opacity, setOpacity] = useState('opacity-0')
-    const [width, setWidth] = useState(`w-${feeling.size}`);
-    const [height, setHeight] = useState(`h-${feeling.size}`);
-    const [scale, setScale] = useState('')
+export default function Mood({feeling, index}: { index: number, feeling: FeelingType }) {
+    const [isHide, setIsHide] = useState(true)
     const router = useRouter();
-    const [transitionPage, setTransitionPage] = useState(false);
     const ref = useRef(null);
     const [isPreview, setIsPreview] = useState(false);
 
     useEffect(() => {
-        setTimeout(() => {
-            setOpacity('opacity-100')
-        }, 300 + (index * 300))
-    }, [])
+        const timeoutId = setTimeout(() => {
+            setIsHide(false);
+        }, 300 + (index * 200))
+        return () => {
+            clearTimeout(timeoutId);
+        }
+    }, []);
 
     useClickOutside(ref, () => {
         setIsPreview(false);
@@ -38,31 +30,24 @@ export default function Mood({feeling, index}: { index: number, feeling: CustomF
     const onMoodClick = () => {
         setIsPreview(true);
     }
+    const enterAnimation = isHide ? `opacity-0 ${index % 2 === 0 ? 'rotate-12' : '-rotate-12'}` : 'opacity-100 rotate-0';
     const imageUrl = `/images/mood_emoji/${feeling.feeling.toLocaleLowerCase()}.png`;
-
-    const defaultClass = `cursor-pointer transition-all ease-in-out rounded-full ${feeling.colors[0]} ${width} ${height} flex justify-center items-center`
+    const defaultClass = `cursor-pointer m-1 transition-all ease-in-out rounded-full ${feeling.colors[0]} w-32 h-32 flex justify-center items-center`
 
     return (
             isPreview ?
-            <div ref={ref} onClick={onPreviewClick}
-                className="m-1"
+            <button ref={ref} onClick={onPreviewClick}
+                className={`animate-wiggle ${defaultClass} flex-col`}
                 >
-                <div className={`animate-wiggle ${defaultClass} flex-col`}>
-                    <span className="text-theme-grey font-bold cursor-pointer text-2xl">{feeling.feeling}</span>
-                    <span className="text-theme-grey">click!</span>
-                        {/* <Image src="/images/arrow-down-icon.svg" width="15" height="10" alt="" /> */}
-                </div>
-            </div>
+                <span className="text-theme-grey font-bold cursor-pointer text-2xl">{feeling.feeling}</span>
+                <span className="text-theme-grey">click!</span>
+            </button>
             :
-            <div onClick={onMoodClick}
-            className="m-1"
-            >
-            <div className={`${opacity} duration-300 hover:rotate-12 hover:scale-110 ${defaultClass}`}>
+            <button onClick={onMoodClick} className={`${enterAnimation} duration-500 hover:rotate-12 hover:scale-110 ${defaultClass}`}>
                 {
                     <Image src={imageUrl} width="50" height="50" alt={feeling.feeling} />
                 }
-            </div>
-        </div>
+            </button>
     )
     
 }
--- pages/moods.tsx
+++ pages/moods.tsx
@@ -1,16 +1,12 @@
 import { GetServerSideProps } from "next";
 import React, { useEffect, useState } from "react";
 import Mood from "../components/listMoods/Mood";
-import { random, shuffle } from "../lib/helper";
+import { shuffle } from "../lib/helper";
 import { isAuth } from "../lib/isAuth";
 import { FeelingType, moodHelper } from "../lib/moodHelper";
+import { Transition } from "@headlessui/react";
 
-export type CustomFeelingType = FeelingType & {
-    size: string;
-    x: string;
-    y: string;
-    set: number;
-}
+
 const groupSize = 5
 
 export const getServerSideProps: GetServerSideProps<{ isLogin: boolean }> = async (context) => {
@@ -31,38 +27,55 @@ export const getServerSideProps: GetServerSideProps<{ isLogin: boolean }> = asyn
 }
 
 const Title = () => {
-    const [status, setStatus] = useState('idle');
+    const [isShow, setIsShow] = useState(false);
     useEffect(() => {
         const timeoutId1 = setTimeout(() => {
-            setStatus('show');
+            setIsShow(true)
         }, 100)
         const timeoutId2 = setTimeout(() => {
-            setStatus('hide')
+            setIsShow(false)
         }, 3000)
         return () => {
             clearTimeout(timeoutId1);
             clearTimeout(timeoutId2);
         }
     }, []);
-    let classStatus = 'opacity-0';
-    if (status === 'show') {
-        classStatus = 'opacity-100 h-40vh';
-    } else if (status === 'hide') {
-        classStatus = 'opacity-0 h-30vh';
-    }
-    const transitionClass = 'transition-all ease-in-out duration-1000';
     return (
-        <div className={`${transitionClass} ${classStatus} flex items-end justify-center`}>
-            <div>
-                <p className="text-3xl font-bold text-theme-blue text-center mt-3 mb-1">How are you feeling <span className="text-4xl">today</span>?</p>
-                <p className="text-2xl text-center mb-3">Let your mood pick the <span className="text-3xl">music</span></p>
+        <Transition
+            show={isShow}
+            as={React.Fragment}
+            enter="transition-all duration-500"
+            enterFrom="opacity-0 h-30vh"
+            enterTo="opacity-100 h-40vh"
+            leave="transition-all duration-1000"
+            leaveFrom="opacity-100 h-40vh"
+            leaveTo="opacity-0 h-30vh"
+        >
+            <div className="flex items-end justify-center">
+                <div>
+                    <p className="text-3xl font-bold text-theme-blue text-center mt-3 mb-1">How are you feeling <span className="text-4xl">today</span>?</p>
+                    <p className="text-2xl text-center mb-3">Let your mood pick the <span className="text-3xl">music</span></p>
+                </div>
             </div>
-        </div>
+        </Transition>
     );
 };
+const groupFeelings = (feelings: FeelingType[]) => {
+    const groups = [];
+    for (let i = 0; i < feelings.length; i += groupSize) {
+        const f = feelings.slice(i, i + groupSize);
+        const g = f.map((feeling, index) => {
+            return {
+                ...feeling,
+            }
+        })
+        groups.push(g);
+    }
+    return groups;
+}
 
 export default function Index(props: { isLogin: boolean }) {
-    const [groupedFeelings, setGroupedFeelings] = useState<CustomFeelingType[][]>([]);
+    const [groupedFeelings, setGroupedFeelings] = useState<FeelingType[][]>([]);
     const [isShow, setIsShow] = useState(false);
     useEffect(() => {
         const f = shuffle(moodHelper.listFeelings());
@@ -76,45 +89,18 @@ export default function Index(props: { isLogin: boolean }) {
             clearTimeout(timeoutId);
         }
     }, [])
-    const groupFeelings = (feelings: FeelingType[]) => {
-        const availableTailwindClass = ['w-28', 'w-32', 'w-36', 'w-40', 'h-28', 'h-32', 'h-36', 'h-40']
-        const availableSizes = ['28', '32']
-        const groups = [];
-        for (let i = 0; i < feelings.length; i += groupSize) {
-            // const sizes = ['28', '28', '32', '32', '36', availableSizes[random(0, availableSizes.length - 1)]]
-            // const sizes = ['32', '32', '36', '36', '36', '40']
-            const sizes = ['32', '32', '32', '32', '32', '32']
-            const x = ['5%', '40%', '85%', '15%', '60%', '95%']
-            const y = ['3%', '0%', '2%', '63%', '45%', '60%']
-            const randomSizes = shuffle(sizes);
-            const f = feelings.slice(i, i + groupSize);
-            const g = f.map((feeling, index) => {
-                return {
-                    ...feeling,
-                    size: randomSizes[index],
-                    x: x[index],
-                    y: y[index],
-                    set: i
-                }
-            })
-            groups.push(g);
-        }
-        return groups;
-    }
     
-    return (<div className="mt-0">
+    return (
+    <div className="mt-0">
         {
             !isShow ?
             <Title />
             :
-            groupedFeelings.map((feelings, index) => (
-                // <div className="flex flex-wrap justify-center relative w-full" style={{height: '280px'}} key={index}>
-                //     { feelings.map((feeling) => <Mood key={feeling.feeling} feeling={feeling} />) }
-                // </div>
-                <div className="flex flex-wrap justify-center relative w-full" key={index}>
-                    { feelings.map((feeling, feelingIndex) => <Mood key={feeling.feeling} index={(index * groupSize) + (feelingIndex)} feeling={feeling} />) }
-                </div>
-            ))
+                    groupedFeelings.map((feelings, index) => (
+                        <div className="flex flex-wrap justify-center relative w-full" key={index}>
+                            { feelings.map((feeling, feelingIndex) => <Mood key={feeling.feeling} index={(index * groupSize) + (feelingIndex)} feeling={feeling} />) }
+                        </div>
+                    ))
         }
     </div>);
 }
